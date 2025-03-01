f5fd00bf206329f71a2e0c6e383cf42ae534deda
b-rucel <b-rucel@users.noreply.github.com>
2025-02-28 20:52:55 -0800

    enabled auth using workers KV store and jwt for locking uploads
    src/handlers/auth.js
      handleLogin
      handleVerify
    
    src/middleware/auth.js
      withAuth will call verifyAuth
    
    withAuth middleware can be wrappped around a route handler to protect it

--- src/types/env.ts
+++ src/types/env.ts
@@ -2,5 +2,6 @@ import { R2Bucket } from '@cloudflare/workers-types';
 
 export interface Env {
   ARTOO_BUCKET: R2Bucket;
-  // Add other environment variables here as needed
+  ARTOO: KVNamespace;
+  JWT_SECRET: string;
 }
